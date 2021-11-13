93ab0f49db8389c1f8eb452b5bcbaf55da3b9b16
zhandosayupov <66684774+zhandosayupov@users.noreply.github.com>
2021-11-13 23:36:09 +0600

    Update Lab7.sql

--- Lab7.sql
+++ Lab7.sql
@@ -39,19 +39,38 @@ create index cur_bal on accounts(currency, balance);
 
 -- 6.
 -- create transaction with "init" status
-begin;
 
-update accounts
-set balance = balance -transactions.amount
-from transactions
-where account_id=transactions.src_account and transactions.status='init'
-and (balance-transactions.amount) >= accounts.limit;
-commit;
-update accounts
-set balance = balance + transactions.amount
-from transactions
-where account_id=transactions.dst_account and transactions.status='init';
-commit ;
-rollback ;
-
-select * from accounts;
+do $$ 
+begin 
+update accounts 
+set balance = balance -transactions.amount 
+from transactions 
+where account_id=transactions.src_account and transactions.status='init'; 
+ 
+ 
+if 0 < (select count(*) from accounts where balance<accounts."limit") then 
+    update accounts 
+    set 
+        balance = balance + transactions.amount 
+    from transactions 
+    where account_id=transactions.src_account and transactions.status='init'; 
+    update transactions 
+    set 
+        transactions.status='rollback' 
+    from accounts 
+    where account_id=transactions.src_account and transactions.status='init'; 
+ 
+    else 
+    update accounts 
+    set balance = balance + transactions.amount 
+    from transactions 
+    where account_id=transactions.dst_account and transactions.status='init'; 
+ 
+    update transactions 
+    set status='commit' 
+    from accounts 
+    where account_id=transactions.dst_account and transactions.status='init'; 
+ 
+    end if; 
+end 
+$$
